44cc8d7ae23f55073f781a5e3cbe5b5ff90dfc9b
yanchanglu <513894105@qq.com>
2016-12-11 17:50:03 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import {Component,OnInit} from '@angular/core';
+import {Component,OnInit,ViewChild,ElementRef} from '@angular/core';
 import * as echarts from 'echarts';
+import { option } from './options';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First App</h1>'
+    template: `<h1>My First App</h1>
+    <div #da style="width:300px;height:300px"></div>
+    `
 })
 
 export class AppComponent implements OnInit{
- ngOnInit() { 
-    console.log(echarts);
-     
+    @ViewChild('da')   da:ElementRef;
+  
+    ngOnInit() { 
+        var myChart = echarts.init(this.da.nativeElement);
+
+        myChart.setOption(option);
     }
 }
